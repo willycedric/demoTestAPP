8c1f308d71108a776b39cb27526a408621e49426
VOLUNTIS\willy.dongmo <willy.dongmo@voluntis.com>
2018-03-07 22:14:24 +0100

    first commit

---
+++ demoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("demoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("demoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f607ef8-e8ca-4528-b769-da5485ae3a40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ demoTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace demoTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestValuesEquals()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestCase]
+        public void TestValuesAreNotEqual()
+        {
+            Assert.AreNotEqual(1, 2);
+        }
+    }
+}
